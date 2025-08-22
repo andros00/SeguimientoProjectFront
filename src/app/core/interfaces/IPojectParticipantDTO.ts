7e81741a07fcff8b064b0c51c6a83e5c52851a5d
andres.tirado <andres.tirado@udea.edu.co>
2025-08-22 13:05:55 -0500

    Cambio formulario

--- src/app/core/interfaces/IPojectParticipantDTO.ts
+++ src/app/core/interfaces/IPojectParticipantDTO.ts
@@ -2,4 +2,13 @@ export interface IProjectParticipantDTO{
    id: String ;
     project: String ;
     responsible: String ;
+    fullName: String;
+    group: Number ;
+    dedicationMonths: Number ;
+    dedicationHours: Number ;
+    dedicationMonthsPlan: Number ;
+    dedicationHoursPlan: Number ;
+    supportedProgramCode: Number ;
+    academicProgPercentage: DoubleRange;
+    projectParticipantRole: [];
 }
