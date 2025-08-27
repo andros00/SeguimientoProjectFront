4f8a189ef11232258ce00f9bf26e98496123445a
johnOcampoDev <bayron.ocampo.8@gmail.com>
2025-08-27 15:15:26 -0500

    Asignacion de formulario inicio formal

--- src/app/shared/components/molecules/formal-start-general-info/formal-start-general-info.component.ts
+++ src/app/shared/components/molecules/formal-start-general-info/formal-start-general-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProjectIformalService } from 'src/app/shared/services/project/project-iformal/project-iformal.service';
+import { IProjectIFormalDTO } from 'src/app/core/interfaces/IProjectIFormalDTO';
 
 @Component({
   selector: 'app-formal-start-general-info',
@@ -10,22 +11,24 @@ import { ProjectIformalService } from 'src/app/shared/services/project/project-i
 export class FormalStartGeneralInfoComponent {
   form!: FormGroup;
 
+  iProject?: IProjectIFormalDTO;
+
   constructor(
     private projectIformalService: ProjectIformalService,
     @Inject('projectCode') public projectCode: string,
     private fb: FormBuilder) {
-    this.initializeForm();
+
   }
 
   private initializeForm(): void {
     this.form = this.fb.group({
-      internCode: ['', Validators.required],
-      projectDuration: [0, Validators.required],
-      startDate: [null, Validators.required],
-      endDate: [null, Validators.required],
-      approvalDate: [null, Validators.required],
-      downloadApprovalDocument: [false, Validators.required],
-      infoApprovalDocument: [false, Validators.required],
+      internCode: [''],
+      projectDuration: [''],
+      startDate: [''],
+      endDate: [''],
+      approvalDate: [''],
+      downloadApprovalDocument: [''],
+      infoApprovalDocument: [''],
     });
   }
 
@@ -35,44 +38,58 @@ export class FormalStartGeneralInfoComponent {
 
     // Recalcular cuando cambie la duración
     this.form.get('projectDuration')!.valueChanges.subscribe(() => this.calculateEndDate());
+
+    this.initializeForm();
+    this.getProjectInicioFormal();
   }
 
   private calculateEndDate(): void {
     console.log('*ingrese*')
     const startDate: Date = this.form.get('startDate')!.value;
-    console.log('*startDate*'+startDate)
+    console.log('*startDate*' + startDate)
     const duration: number = parseInt(this.form.get('projectDuration')!.value, 10);
-    console.log('*duration*'+duration)
+    console.log('*duration*' + duration)
 
-  if (startDate && !isNaN(duration) && duration > 0) {
-    const endDate = new Date(startDate);
-    endDate.setMonth(endDate.getMonth() + duration);
-    this.form.get('endDate')!.setValue(endDate, { emitEvent: false });
-    console.log('endDate'+endDate)
+    if (startDate && !isNaN(duration) && duration > 0) {
+      const endDate = new Date(startDate);
+      endDate.setMonth(endDate.getMonth() + duration);
+      this.form.get('endDate')!.setValue(endDate, { emitEvent: false });
+      console.log('endDate' + endDate)
+    }
   }
-}
 
-onStartDateChange(date: Date): void {
-  console.log(date)
-  this.form.get('startDate')!.setValue(date);
-  this.calculateEndDate(); // haces el cálculo justo después de actualizar
-}
+  onStartDateChange(date: Date): void {
+    console.log(date)
+    this.form.get('startDate')!.setValue(date);
+    this.calculateEndDate(); // haces el cálculo justo después de actualizar
+  }
 
-getProjectInicioFormal(): void {
+  getProjectInicioFormal(): void {
     this.projectIformalService.getProjectIFormalByProjectCode(this.projectCode).subscribe({
+
       next: (data) => {
-        if (data && data.length > 0) {
-          this.projectIformalService.data = data;
-          this.noParticipants = false;
-        } else {
-          this.noParticipants = true;
-          this.participants.data = [];
-        }
+        this.iProject = data;
+        this.form.patchValue({
+          internCode: data.projectCode,
+          projectDuration: data.projectDuration,
+          startDate: data.startDate ? new Date(data.startDate) : null,
+          endDate: data.endDate ? new Date(data.endDate) : null,
+          approvalDate: data.approvalDate ? new Date(data.approvalDate) : null,
+          downloadApprovalDocument: data.downloadApprovalDocument,
+          infoApprovalDocument: data.infoApprovalDocument
+        });
       },
       error: (err) => {
-        console.error('Error fetching participants', err);
+        if (err.status === 404) {
+          console.error(`No se encontró un proyecto con el código ${this.projectCode}`);
+        } else {
+          console.error('Ocurrió un error inesperado');
+        }
+        this.iProject = undefined;
       }
     });
+
+
   }
 
 }
